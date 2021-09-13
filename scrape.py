de8535b67b2e5cd81268d6d10749ff977a439e39
pokgak <m.aimanismail@gmail.com>
2021-09-14 01:23:37 +0800

    prepend slug name to data files

--- scrape.py
+++ scrape.py
@@ -9,8 +9,8 @@ with open("targets.csv", newline="") as targetsfile:
     targets = csv.reader(targetsfile, delimiter=",")
     targets.__next__()
     for row in targets:
-        r = requests.get(API_ENDPOINT, params={"itemid": row[0], "shopid": row[1]})
+        r = requests.get(API_ENDPOINT, params={"itemid": row[1], "shopid": row[2]})
 
         result = jq.compile(".data").input(text=r.text).all()
-        with open(f"data/{row[0]}-{row[1]}.json", "w", encoding="utf-8") as f:
+        with open(f"data/{row[0]}_{row[1]}-{row[2]}.json", "w", encoding="utf-8") as f:
             json.dump(result, f, indent=2)
